e7f1036e688e2dd1508eeeb08ed4331c16824285
Aidan Z <aidanxyz@gmail.com>
2013-02-03 12:34:53 +0600

    no feedback_views; everyting points to views.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('reviews',
-	url(r'^feedback/add/$', 'feedback_views.add'),
+urlpatterns = patterns('reviews.views',
+	url(r'^feedback/add/$', 'add_feedback'),
+	url(r'^vote/$', 'vote'),
+	url(r'^unvote/$', 'unvote'),
 )
